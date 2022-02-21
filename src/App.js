b5572868c86536dd9e66fcde89ea213705fa4b47
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-02-21 12:49:52 +0000

    Add store, remove and create actions

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import BookSection from './components/BookSection';
 import Categories from './components/Categories';
 import Form from './components/Form';
 import Navbar from './components/Navbar';
+import store from './redux/configureStore';
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route
-          exact
-          path="/"
-          element={(
-            <>
-              <BookSection />
-              <Form />
-            </>
-          )}
-        />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={(
+              <>
+                <BookSection />
+                <Form />
+              </>
+            )}
+          />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,44 @@
-const Form = () => (
-  <form className="book-form" required>
-    <label htmlFor="book">
-      ADD NEW BOOK
-      <br />
-      <input type="text" id="book" placeholder="Book title" required />
-    </label>
-    <select name="categories" id="categories" required>
-      <option value="" disabled selected>
-        Category
-      </option>
-      <option value="education">Education and Teaching</option>
-      <option value="history">History</option>
-      <option value="entertainment">Humor and Entertainment</option>
-      <option value="mystery">Mystery, Thriller and Suspense</option>
-      <option value="romance">Romance</option>
-      <option value="fantasy">Science Fiction and Fantasy</option>
-    </select>
-    <button className="btn" type="submit">
-      ADD BOOK
-    </button>
-  </form>
-);
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
+import store from '../redux/configureStore';
 
+const Form = () => {
+  const dispatch = useDispatch();
+
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: store.getState().bookReducer.length,
+      title: e.target[0].value,
+      author: e.target[1].value,
+      category: e.target[2].value,
+    };
+
+    dispatch(addBook(newBook));
+  };
+  return (
+    <form onSubmit={(e) => submitBookToStore(e)} className="book-form" required>
+      <label htmlFor="book">
+        ADD NEW BOOK
+        <br />
+        <input type="text" id="book" placeholder="Book title" required />
+      </label>
+      <input type="text" id="author" placeholder="Book author" required />
+      <select defaultValue="" name="categories" id="categories" required>
+        <option value="" disabled>
+          Category
+        </option>
+        <option value="education">Education and Teaching</option>
+        <option value="history">History</option>
+        <option value="entertainment">Humor and Entertainment</option>
+        <option value="mystery">Mystery, Thriller and Suspense</option>
+        <option value="romance">Romance</option>
+        <option value="fantasy">Science Fiction and Fantasy</option>
+      </select>
+      <button className="btn" type="submit">
+        ADD BOOK
+      </button>
+    </form>
+  );
+};
 export default Form;
